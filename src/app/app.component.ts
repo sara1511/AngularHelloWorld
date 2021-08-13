2c275a4745d85cc64282ef93fa305e2db910a775
Saradindu Sarkar <sarkarsa@microsoft.com>
2021-08-13 16:47:44 +0530

    updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, Injectable, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+@Injectable()
+export class AppComponent implements OnInit {
   title = 'hello-world';
+  constructor(private http: HttpClient){ }
+
+  ngOnInit() {
+    this.fetchAll();
+  }
+
+  fetchAll(){
+    this.http.get('https://angularlinuxdemoapp-node-express-multer.azurewebsites.net/')
+    .subscribe(response=>{console.log(response)}, err=>{console.log("Service Not Found"+ err)})
+  }
 }
